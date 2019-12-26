ed3ae82630cbbe660e087351804417012d1f2cb8
jwayn <prickettjw@gmail.com>
2019-12-26 01:28:05 -0800

    Fixed styling on overall app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="w-full mt-12 sm:mt-0 bg-gray-100">
+  <div id="app" class="w-full min-h-screen mt-12 sm:mt-0 bg-gray-100">
     <Banner/>
     <div class="flex justify-center w-full">
       <InitiativeScreen/>
