b8c73bc41e5bf774ef9d6f1e001f4ce962c938ae
jwayn <prickettjw@gmail.com>
2019-12-28 21:39:37 -0800

    Changed verify-token return name to tokenData

--- backend/middleware/verify-token.js
+++ backend/middleware/verify-token.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        req.userData = decodedToken;
+        req.tokenData = decodedToken;
     } catch (err) {
         console.log(err);
         return res.status(401).json({
